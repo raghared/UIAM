4dac91159befc0dbe3a7e4f31d3c6ab7207fd15d
raghared <37149698+raghared@users.noreply.github.com>
2020-03-03 22:49:26 +0530

    added comments.

--- config/src/main/java/com/piggymetrics/SecurityConfig.java
+++ config/src/main/java/com/piggymetrics/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.piggymetrics;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+//jhasdjfhd
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
